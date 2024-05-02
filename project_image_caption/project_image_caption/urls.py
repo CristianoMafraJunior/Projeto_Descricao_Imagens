022e2355fbe0246e91ca7ae8fc907940874fa2f7
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-05-02 19:37:18 -0300

    Add HTML e CSS

--- project_image_caption/project_image_caption/urls.py
+++ project_image_caption/project_image_caption/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
 from app_image_caption import views
+from django.urls import path
 
 urlpatterns = [
     # rota, view responsável, nome de referencia
-    path('',views.home,name='home'),
-
+    path("", views.home, name="home"),
 ]
